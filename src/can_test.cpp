b61d321930d17a2b68fc0fa6aefa3fd9e776635f
ChaMyeongju <79961300+ChaMyeongju@users.noreply.github.com>
2023-11-06 13:16:25 +0900

    Update can_test.cpp
    subscribing keyboard published topic part is added

--- src/can_test.cpp
+++ src/can_test.cpp
@@ -4,10 +4,80 @@
 #include "cmath"
 #include <iostream>
 
+#include "can_driver_t/control_param.h"
+// #include "can_driver_t/control_param_srv.h"
+
 using namespace std;
 
 #define MOTOR_ID 1
 
+double *reference_pos, *reference_speed;
+double *kp_val, *kd_val;
+double *target_torque;
+int number_drivers;
+
+
+void control_param_subscribe(const can_driver_t::control_param::ConstPtr& msg)
+{
+  cout << "msg recieved" << endl;
+  if (msg->control_mode == 1)
+  {
+    for (int i=0; i<number_drivers; i++)
+    {
+      reference_pos[i] = msg->ref_pos;
+    }    
+  }
+  else if (msg->control_mode == 2)
+  {
+    for (int i=0; i<number_drivers; i++)
+    {
+      reference_speed[i] = msg->ref_vel;
+    }    
+  }
+  else if (msg->control_mode == 3)
+  {
+    for (int i=0; i<number_drivers; i++)
+    {
+      reference_pos[i] = msg->ref_pos;
+      reference_speed[i] = msg->ref_vel;
+    }    
+  }
+  else if (msg->control_mode == 4)
+  {
+    for (int i=0; i<number_drivers; i++)
+    {
+      kp_val[i] = msg->kp;
+    }    
+  }
+  else if (msg->control_mode == 5)
+  {
+    for (int i=0; i<number_drivers; i++)
+    {
+      kd_val[i] = msg->kd;
+    }    
+  }
+  else if (msg->control_mode == 6)
+  {
+    for (int i=0; i<number_drivers; i++)
+    {
+      kp_val[i] = msg->kp;
+      kd_val[i] = msg->kd;
+    }    
+  }
+}
+
+// bool control_param_setting(can_driver_t::control_param_srv::Request &req, can_driver_t::control_param_srv::Response &res)
+// {
+//   res.stop_flag     = req.stop_flag;   
+//   res.init_flag     = req.init_flag;  
+//   res.control_mode  = req.control_mode; 
+//   res.ref_pos       = req.ref_pos;      
+//   res.ref_vel       = req.ref_vel;     
+//   res.kp            = req.kp;          
+//   res.kd            = req.kd;
+
+//   return true;          
+// }
 
 Motor_Type Motor_type_setting(int motor_type_flag)
 {
@@ -29,6 +99,11 @@ int main(int argc, char **argv)
 {
   ros::init(argc,argv,"can_test");
   ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe<can_driver_t::control_param>("/control_param_msg", 1000, control_param_subscribe);
+
+  int display_motor_state_flag;
+  ros::param::get("/display/motor_state_flag",display_motor_state_flag);
+
 
   double rate;
   ros::param::get("/ros_param/rate",rate);
@@ -39,7 +114,6 @@ int main(int argc, char **argv)
 
   int clock = 0;
 
-  int number_drivers;
   int *current_driver_IDs;
   Motor_Type *motor_types;
   Drive_Mode *drive_modes;
@@ -70,9 +144,6 @@ int main(int argc, char **argv)
   can_base CanDriver(number_drivers, current_driver_IDs, motor_types, drive_modes);
 
 
-  double *reference_pos, *reference_speed;
-  double *kp_val, *kd_val;
-  double *target_torque;
   reference_pos = new double[number_drivers];
   reference_speed = new double[number_drivers];
   kp_val = new double[number_drivers];
@@ -114,12 +185,14 @@ int main(int argc, char **argv)
   while(ros::ok()){
     // sleep(1);
 
+    t_start = ros::Time::now().toSec();
 
     // CanDriver.TargetPosition(reference);
-    t_start = ros::Time::now().toSec();
     CanDriver.Recieve_state(answer);
+
     t_end = ros::Time::now().toSec();
     t_diff = t_start - t_end;
+
 
     for(int i = 0; i < number_drivers; i++)
     {
@@ -132,7 +205,8 @@ int main(int argc, char **argv)
       CanDriver.TargetTorqueWithPD(target_torque, kp_val, reference_pos, kd_val, reference_speed);
 
     // ROS_INFO("real torque : %f || target torque : %f || position : %f",answer[0].torque,target_torque[0],answer[0].position);
-    ROS_INFO("time : %f || position : %f || speed : %f || torque : %f || temperature : %d",t_diff,answer[0].position,answer[0].speed,answer[0].torque,answer[0].motor_temperature);
+    if (display_motor_state_flag == 1)
+      ROS_INFO("time : %f || position : %f || speed : %f || torque : %f || temperature : %d",t_diff,answer[0].position,answer[0].speed,answer[0].torque,answer[0].motor_temperature);
     // cout << "Position : " << answer[0].position << " speed : " << answer[0].speed << " torque : " << answer[0].torque << " temperature : " << answer[0].motor_temperature << endl;
     ros::spinOnce();
     loop_rate.sleep();
